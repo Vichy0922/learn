c41a6bac6180ac38d2de154fffc1e500f6d32f1c
Vichy0922 <1447159691@qq.com>
2019-09-30 20:21:42 +0800

    update the index ftl file

--- src/main/java/edu/hubu/learn/web/MusicController.java
+++ src/main/java/edu/hubu/learn/web/MusicController.java
@@ -1,37 +1,45 @@
 package edu.hubu.learn.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import edu.hubu.learn.entity.User;
+import edu.hubu.learn.service.UserService;
 
 import edu.hubu.learn.entity.Music;
 import edu.hubu.learn.service.MusicService;
 
 @Controller
-@RequestMapping("/")
-public class IndexController {
+@RequestMapping("/music")
+public class MusicController {
 
-   
 
-    @RequestMapping("/")
-    public ModelAndView index() {
-        ModelAndView mav = new ModelAndView();
-        mav.setViewName("index");
-        return mav;
-    }
-
-   
     @Autowired
     private MusicService musicService;
 
-    @RequestMapping("/music")
-    public ModelAndView music() {
+    @RequestMapping("/{id}")
+    public ModelAndView music(@PathVariable Long id) {
         ModelAndView mav = new ModelAndView();
         Music music = musicService.getMusic(1l);
         mav.addObject("music", music);
         mav.setViewName("user");
         return mav;
     }
+
+    @RequestMapping("/list")
+    public ModelAndView musics(){
+        ModelAndView mav = new ModelAndView();
+        List<Music> musics = musicService.getMusics();
+        mav.addObject("musics", musics);
+        mav.setViewName("musics");
+        return mav;
+
+    }
+
+
 }
---
+++ src/main/resources/templates/musics.ftl
@@ -0,0 +1,12 @@
+<html>
+<head></head>
+<body>
+<table>
+<#list musics as music123>
+<tr>
+<td>${music123.songname}</td><td>${music123.singer}</td>
+</tr>
+</#list>
+</table>
+</body>
+</html>
